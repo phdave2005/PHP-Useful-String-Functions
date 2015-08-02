4ea00788c9c00c9a28c6b6df0c4dd5fe8c507b6d
David Partyka <phdave2005@gmail.com>
2015-08-02 12:54:45 -0400

    Update str_rclip.php

--- str_rclip.php
+++ str_rclip.php
@@ -9,17 +9,18 @@ function str_rclip($str, $clipStr, $multiple) {
     
     if ($clippedStrLength > 0 && $clipLength > 0 && ($clipLength <= $clippedStrLength) && strpos($clippedStr, $clipStr) !== false) {
 		
-	      $maxIterations = ($multiple === true) ? ceil($clippedStrLength / $clipLength) : 1;
+	$maxIterations = ($multiple === true) ? ceil($clippedStrLength / $clipLength) : 1;
 		
       	for ($i = 0; $i < $maxIterations; $i++) {
 			
-			      $reverseStr = strrev($clippedStr);
-		      	$reverseClipStr = strrev($clipStr);
+            $reverseStr = strrev($clippedStr);
+            $reverseClipStr = strrev($clipStr);
                   
-		      	if (strpos($reverseStr, $reverseClipStr) === 0) $clippedStr = strrev(substr($reverseStr, $clipLength, strlen($clippedStr)));
+            if (strpos($reverseStr, $reverseClipStr) === 0) $clippedStr = strrev(substr($reverseStr, $clipLength, strlen($clippedStr)));
+            
             if (strpos($clippedStr, $clipStr) === false) break;
             
-	      } 
+	} 
     }
     
     return $clippedStr;
