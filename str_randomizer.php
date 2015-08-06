e7b4144817bd81e8ed5e89e83180334554501a38
David Partyka <phdave2005@gmail.com>
2015-08-06 06:21:53 -0400

    Create str_randomizer.php

---
+++ str_randomizer.php
@@ -0,0 +1,48 @@
+<?php
+
+function str_randomize($str) {
+	
+	  $strLength = strlen($str);
+	  $strMultiple = 100 * $strLength;
+	  $keyvalArray = $numberArray = $tempArray = array();
+	  $randomizedStr = '';
+	
+	  for($i = 0; $i < $strLength; $i++) {
+				$rand = mt_rand(1, $strMultiple);
+		    $keyvalArray += array($str[$i] => $rand);
+	  }
+	
+	  $counter = 0;
+	  foreach($keyvalArray as $key=>$value) {
+		    $tempStr = '';
+		    $tempArray = array();
+		    if ($counter > 0) {
+			      for($j = 0; $j < $counter; $j++) {
+				        if ($value >= $numberArray[$j]) {
+					          $tempStr = $randomizedStr;
+					          $tempArray = $numberArray;
+					          for($k = $j; $k < $counter; $k++) {
+						            $randomizedStr[$k + 1] = $tempStr[$k];
+					              $numberArray[$k + 1] = $tempArray[$k];	
+					          }
+					          $randomizedStr[$j] = $key;
+					          $numberArray[$j] = $value;
+					          break;
+				        } else if ($j == ($counter - 1)) {
+					          $randomizedStr .= $key;
+					          $numberArray[] = $value;
+				        } else ;			
+			      }			
+		    } else {
+		        $randomizedStr = $key;
+			      $numberArray[] = $value;
+		    }
+		    $counter++;
+	  }
+  	return $randomizedStr;
+}
+
+
+
+
+?>
